725aadd3058d273a2e63a39d04ca6a00cf4d5704
yaoandy107 <yaoandy107@gmail.com>
2020-07-15 12:20:25 +0800

    Add custom info window and refactor code

--- app/src/main/java/me/yaoandy107/triplecoupon/ui/map/MapFragment.kt
+++ app/src/main/java/me/yaoandy107/triplecoupon/ui/map/MapFragment.kt
@@ -2,33 +2,34 @@ package me.yaoandy107.triplecoupon.ui.map
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.Typeface
 import android.location.Location
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.fragment_map.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import me.yaoandy107.triplecoupon.R
+import me.yaoandy107.triplecoupon.model.Store
 import me.yaoandy107.triplecoupon.ui.store.StoreViewModel
+
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private val storeViewModel: StoreViewModel by activityViewModels()
-    private var markerOptionsList: List<MarkerOptions>? = null
     private var map: GoogleMap? = null
 
     override fun onCreateView(
@@ -74,22 +75,10 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        map?.setMapStyle(
-            MapStyleOptions.loadRawResourceStyle(
-                requireContext(), R.raw.style_json
-            )
-        )
-        storeViewModel.stores.value?.forEach {
-            val options = MarkerOptions()
-                .position(LatLng(it.latitude.toDouble(), it.longitude.toDouble()))
-                .title(it.storeNm)
-                .snippet("庫存：${it.total}")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-            if (it.total.toInt() <= 0) {
-                options.alpha(0.3f)
-            }
-            map!!.addMarker(options)
-        }
+        setupLocationEnable()
+        setupMapStyle()
+        setupInfoWindowAdapter()
+        setupMarkers()
     }
 
     private fun setupLocationService() {
@@ -111,4 +100,74 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+
+    private fun setupLocationEnable() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map?.isMyLocationEnabled = true
+        }
+    }
+
+    private fun setupMapStyle() {
+        map?.setMapStyle(
+            MapStyleOptions.loadRawResourceStyle(
+                requireContext(), R.raw.style_json
+            )
+        )
+    }
+
+    private fun setupInfoWindowAdapter() {
+        map?.setInfoWindowAdapter(object : InfoWindowAdapter {
+            override fun getInfoContents(marker: Marker?): View {
+                val infoView = LinearLayout(requireContext())
+                infoView.orientation = LinearLayout.VERTICAL
+
+                val title = TextView(requireContext())
+                title.text = marker?.title
+                title.setTextColor(Color.BLACK)
+                title.setTypeface(null, Typeface.BOLD)
+                title.gravity = Gravity.CENTER
+
+                val snippet = TextView(requireContext())
+                snippet.text = marker?.snippet
+                snippet.setTextColor(Color.GRAY)
+
+                infoView.addView(title)
+                infoView.addView(snippet)
+
+                return infoView
+            }
+
+            override fun getInfoWindow(p0: Marker?): View? {
+                return null
+            }
+
+        })
+    }
+
+    private fun setupMarkers() {
+        storeViewModel.stores.value?.forEach { store ->
+            setupMarker(store)
+        }
+    }
+
+    private fun setupMarker(store: Store) {
+        val regex = Regex(pattern = "[0-9]{2}:[0-9]{2}-[0-9]{2}:[0-9]{2}")
+        val businessTime = regex.find(store.busiTime)?.value
+        val options = MarkerOptions()
+            .position(LatLng(store.latitude.toDouble(), store.longitude.toDouble()))
+            .title(store.storeNm)
+            .snippet("庫存：${store.total}\n時間：$businessTime")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        if (store.total.toInt() <= 0) {
+            options.alpha(0.3f)
+        }
+        map!!.addMarker(options)
+    }
 }
